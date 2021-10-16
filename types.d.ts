d2947f51764e349fa51435d58c62a8134a3ea34d
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-10-16 13:38:57 -0400

    0.0.18

--- types.d.ts
+++ types.d.ts
@@ -4,53 +4,53 @@ export interface IObserve{
     /**
      * A css match criteria, used in an "upsearch" for the element to observe.
      */
-    observe: string,
+    observe?: string,
     /**
      * Observe first ancestoer DOM element matching this string
      */
-    observeClosest: string,
+    observeClosest?: string,
     /**
      * Event name to watch for
      */
-    on: string,
+    on?: string,
     /**
      * The path to the (sub) property of the element being observed.
      * 
      */
-    valFromTarget: string,
+    valFromTarget?: string,
     /**
      * Abbreviation for valFromTarget.  Does the same thing
      */
-    vft: string,
+    vft?: string,
     /**
      * The path to the place in the event we want to use as the value to set.  
      * For example:  detail.value
      */
-    valFromEvent: string,
+    valFromEvent?: string,
     /**
      * Abbreviation for vfe.  Does the same thing.
      */
-    vfe: string,
+    vfe?: string,
     /**
      * Perform a structural clone before passing the observed value.
      */
-    clone: boolean,
+    clone?: boolean,
     /** Do not hook the initial value prior to any events being fired. */
-    skipInit: boolean,
+    skipInit?: boolean,
     /**
      *  'int' | 'float' | 'bool' | 'date' | 'truthy' | 'falsy' | '' | 'string' | 'object';  
      */
-    parseValAs: string,
+    parseValAs?: string,
     /**
      * Subscribe to property changes rather to events.
      */
-    onSet: string,
+    onSet?: string,
     /** Set attribute rather than property. */
-    as: 'str-attr' | 'bool-attr' | 'obj-attr',
+    as?: 'str-attr' | 'bool-attr' | 'obj-attr',
     /** If val is true, set property to this value. */
-    trueVal: any,
+    trueVal?: any,
     /** If val is false, set property to this value. */
-    falseVal: any,
+    falseVal?: any,
 }
 
 
