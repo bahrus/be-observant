c46a1c526426b93489da167072215ce4d3d18021
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-15 05:57:04 -0400

    0.0.151

--- be-observant.ts
+++ be-observant.ts
@@ -1,4 +1,4 @@
-import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {define, BeDecoratedProps} from 'be-decorated/DE.js';
 import {Actions, IObserve, VirtualProps, HookUpInfo, PP, Proxy} from './types';
 import {register} from "be-hive/register.js";
 
