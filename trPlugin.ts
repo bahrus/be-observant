4d147550c65f06124538816f24c4d383e08d808a
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-25 07:47:57 -0400

    0.0.155

--- trPlugin.ts
+++ trPlugin.ts
@@ -1,38 +1,16 @@
 import {RenderContext, TransformPluginSettings} from 'trans-render/lib/types';
+import {DEMethods} from 'be-decorated/types';
 import {register} from 'trans-render/lib/pluginMgr.js';
-import { hookUp } from './hookUp.js';
-import { VirtualProps } from './types.js';
 
 export const trPlugin: TransformPluginSettings = {
-    selector: 'beObservantAttribs',
+    selector: 'beObservableAttribs',
     ready: true,
-    processor: async ({target, val, attrib, host}: RenderContext) => {
-        const params = JSON.parse(val!);
-        const fulfilled = [];
-        const unfulfilled = [];
-        for(const propKey in params){
-            const parm = params[propKey];
-            if(await hookUp(parm, target! as Element & VirtualProps, propKey, true, host)){
-                fulfilled.push(propKey);
-            }else{
-                unfulfilled.push(propKey);
-            }
-        }
-        if(unfulfilled.length === 0){
-            target!.setAttribute(attrib!.replace('be-', 'is-'), val!);
-            target!.removeAttribute(attrib!);
-        }else{
-            const isObj = {} as any;
-            const beObj = {} as any;
-            for(const propKey of fulfilled){
-                isObj[propKey] = params[propKey];
-            }
-            for(const propKey of unfulfilled){
-                beObj[propKey] = params[propKey];
-            }
-            target!.setAttribute(attrib!, JSON.stringify(beObj));
-            target!.setAttribute(attrib!.replace('be-', 'is-'), JSON.stringify(isObj));
-        }        
+    processor:  async (ctx:  RenderContext) => {
+        const {target} = ctx;
+        if(customElements.get('be-observant') === undefined) return;
+        const {attach} = await import('be-decorated/upgrade.js');
+        const instance = document.createElement('be-observant') as any as DEMethods;
+        attach(target!, 'clonable', ctx, instance.attach.bind(instance));
     }
 }
 
