c69291660c87119f136bc030e37310331760fab0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-13 17:09:00 -0400

    0.0.164

---
+++ be-observant.ts
@@ -0,0 +1,63 @@
+import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
+import {BEConfig} from 'be-enhanced/types';
+import {XE} from 'xtal-element/XE.js';
+import {Actions, AllProps, AP, PAP, ProPAP, POA} from './types';
+import {register} from 'be-hive/register.js';
+import {ElTypes, SignalInfo} from 'be-linked/types';
+
+export class BeObservant extends BE<AP, Actions> implements Actions{
+    #abortControllers: Array<AbortController>  = [];
+    detach(): void {
+        for(const ac of this.#abortControllers){
+            ac.abort();
+        }
+    }
+    static override get beConfig(){
+        return {
+            parse: true,
+            parseAndCamelize: true,
+            isParsedProp: 'isParsed'
+        } as BEConfig;
+    } 
+    
+    async onCamelized(self: this) {
+        return {
+
+        }
+    }
+
+    async hydrate(self: this){
+        return {
+            resolved: true,
+        }
+    }
+}
+
+export interface BeObservant extends AllProps{}
+
+const tagName = 'be-observant';
+const ifWantsToBe = 'observant';
+const upgrade = '*';
+
+const x = new XE<AP, Actions>({
+    config:{
+        tagName,
+        isEnh: true,
+        propDefaults: {
+            ...propDefaults,
+        },
+        propInfo: {
+            ...propInfo,
+        },
+        actions: {
+            onCamelized: {
+                ifAllOf: ['isParsed'],
+                ifAtLeastOneOf: ['of', 'Of']
+            },
+            hydrate: 'observeRules'            
+        }
+    },
+    superclass: BeObservant
+});
+
+register(ifWantsToBe, upgrade, tagName);
---
+++ types.d.ts
@@ -0,0 +1,31 @@
+import { ActionOnEventConfigs } from "trans-render/froop/types";
+import {IBE} from 'be-enhanced/types';
+
+export interface EndUserProps extends IBE{
+    Of?: Array<OfStatement>,
+    of?: Array<OfStatement>
+} 
+
+export interface AllProps extends EndUserProps{
+    isParsed?: boolean,
+    observeRules?: Array<ObserveRule>,
+}
+
+export type OfStatement = string;
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
+
+export interface Actions{
+    onCamelized(self: this): ProPAP;
+    hydrate(self: this): ProPAP;
+}
+
+export interface ObserveRule{
+
+}
