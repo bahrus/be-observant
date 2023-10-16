1b4ca17d551a43ab80c13c34098fb73cf4e1f6d0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-16 06:17:36 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-observant into baseline

--- MyCustomElement/MyCustomElement.ts
+++ MyCustomElement/MyCustomElement.ts
@@ -32,7 +32,7 @@ export class MyCustomElement extends HTMLElement{
             <div id=isVegetarian></div>
             <div id=age></div>
             <my-peer-element -some-bool-prop></my-peer-element>
-            <!-- <h3>Example 1a</h3>
+            <h3>Example 1a</h3>
             <input name=isVegetarian type=checkbox onclick="return false;" be-observant>
             <h3>Example 1b</h3>
             <input type=checkbox onclick="return false;" be-observant='of /is vegetarian.'>
@@ -51,7 +51,7 @@ export class MyCustomElement extends HTMLElement{
             <input id=searchString type=search>
             <div be-observant='of #search string.'></div>
             <h3>Example 2c</h3>
-            <input type=checkbox onclick="return false" be-observant='of -some-bool-prop'> -->
+            <input type=checkbox onclick="return false" be-observant='of -some-bool-prop'>
             <input name=someCheckbox type=checkbox>
 
             <my-peer-element enh-by-be-observant='of @ some checkbox and assign to some bool prop'></my-peer-element>
