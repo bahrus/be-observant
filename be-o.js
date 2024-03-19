56e02eb641110d03302a0e213df13dafda7fe485
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-19 06:34:52 -0400

    0.0.176

---
+++ be-o.js
@@ -0,0 +1,6 @@
+import { register } from 'be-hive/register.js';
+import { tagName } from './be-observant.js';
+import './be-observant.js';
+const ifWantsToBe = 'o';
+const upgrade = '*';
+register(ifWantsToBe, upgrade, tagName);
