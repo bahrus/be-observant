d32c3904f22e025dcb85f0e08a18c56c47f772e0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-17 05:55:20 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-observant into baseline

--- legacy/be-o.ts
+++
@@ -1,8 +0,0 @@
-import {register} from 'be-hive/register.js';
-import {tagName } from './be-observant.js';
-import './be-observant.js';
-
-const ifWantsToBe = 'o';
-const upgrade = '*';
-
-register(ifWantsToBe, upgrade, tagName);
