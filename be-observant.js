85ea9720273f66e5a6031c3f2a6e9033569319e3
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-19 07:49:53 -0400

    Update README.md

--- be-observant.js
+++ be-observant.js
@@ -12,10 +12,46 @@ import { propInfo } from 'be-enhanced/cc.js';
  */
 class BeObservant extends BE {
     /**
-     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement, any>}
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
      */
     static config = {
+        propDefaults: {
+            
+        },
+        propInfo: {
+            ...propInfo,
+            parsedStatements: {},
+            customHandlers: {},
+            scopedCustomHandlers: {},
+        }
+    }
 
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async noAttrs(self){
+        const {enhancedElement} = self;
+        const {getDefaultRemotePropName} = await import('trans-render/asmr/getDefaultRemotePropName.js');
+        const specifier = {
+            s: '/',
+            elS: '*',
+            dss: '^',
+            scopeS: '[itemscope]',
+            rec: true,
+            rnf: true,
+            prop: getDefaultRemotePropName(enhancedElement),
+            host: true
+        }
+        const parsedStatement : ObservingParameters = {
+            remoteSpecifiers: [specifier],
+            aggregateRemoteVals: this.#hasOnload ? 'ObjectAssign' : 'Conjunction'
+        };
+        return /** @type {PAP} */({
+            didInferring: true,
+            parsedStatements: [parsedStatement]
+        });
     }
 
     async handleEvent() {
