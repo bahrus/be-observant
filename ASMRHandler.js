48a31c98ccf981638021067f5cc1a8fd097bf55f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-20 07:27:35 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-observant into baseline

--- ASMRHandler.js
+++ ASMRHandler.js
@@ -7,14 +7,44 @@
  */
 export class ASMRHandler{
     /**
+     * @type {Handlers}
+     */
+    #self
+
+    /**
+     * @type {SharingObject}
+     */
+    #localSharingObject;
+
+    /**
+     * @type {{[key: string] : AbsorbingObject}}
+     */
+    #propToAO;
+
+    /**
+     * @type {AbortController | undefined}
+     */
+    #ac;
+
+    /**
      * 
      * @param {Handlers} self 
      * @param {SharingObject} localSharingObject 
      * @param {{[key: string] : AbsorbingObject}} propToAO 
      */
     constructor(self, localSharingObject, propToAO){
-
+        this.#self = self;
+        this.#localSharingObject = localSharingObject;
+        this.#propToAO = propToAO;
+        const ac = this.#ac =  new AbortController;
+        const aos = Object.values(propToAO);
+        for(const ao of aos){
+            ao.addEventListener('.', this, {signal: ac.signal})
+        }
+        this.handleEvent();
     }
+
+
 
     handleEvent() {
         throw new Error("Method not implemented.");
